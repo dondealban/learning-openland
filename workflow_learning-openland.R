949b20b126bdee6761a1e28e931d2ccde725a9f2
dondealban <dondealban@gmail.com>
2021-03-08 13:50:05 +0800

    Initial commit

---
+++ workflow_learning-openland.R
@@ -0,0 +1,9 @@
+# This script implements a land cover change analysis workflow using the Intensity 
+# Analysis framework via the intensity.analysis R package (Pontius & Khallaghi 2019).
+# The script uses land cover image datasets from one of my land cover change studies
+# investigating land-cover regime shifts in Tanintharyi Region, Myanmar, published 
+# in Sustainability journal (https://doi.org/10.3390/su11041139).
+# 
+# Script modified by: Jose Don T. De Alban
+# Date created:       30 May 2019
+# Date modified:      13 Jun 2019  
